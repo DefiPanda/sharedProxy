6d290797e8fbb1bea2e3acbcf02268c007995336
jw2013 <zhew@live.unc.edu>
2014-03-02 02:35:04 -0500

    aws config

--- 404.php
+++ 404.php
@@ -1,16 +1,25 @@
 <?php
 $ch = curl_init();
 $fields = http_build_query($_POST);
+$URI = $_SERVER['REQUEST_URI'];
+$pos1 = strpos($URI, '/');
+$pos2 = strpos($URI, '/', $pos1 + strlen('/'));
+$tokens = explode('file=', $_SERVER['HTTP_REFERER']);
 
-curl_setopt($ch,CURLOPT_URL,'https://wwwx.cs.unc.edu/~duozhao/projects/candyGame/connector.php');
-
-  curl_setopt($ch,CURLOPT_POSTFIELDS,$fields);
-  curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
-  curl_setopt($ch,CURLOPT_TIMEOUT, 20);
-
-$response = curl_exec($ch);
-header('Content-type: application/json');
-print(json_encode($response));
-curl_close ($ch);
+if(count($tokens) == 2){
+	$filename = trim($tokens[1]);
+	$filepath = 'api/shared/'.$filename.'/host.txt';
+	$prefix = file_get_contents($filepath);
+	$postfix = substr($URI, $pos2);
+	$url = $prefix.$postfix;
+	curl_setopt($ch,CURLOPT_URL, $url);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$fields);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
+    curl_setopt($ch,CURLOPT_TIMEOUT, 20);
+	$response = curl_exec($ch);
+	header('Content-type: application/json');
+	print(json_encode($response));
+	curl_close ($ch);
+}
 ?>
