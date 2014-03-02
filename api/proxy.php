72e003e6bb23c2680f69cacc8553758d3810f066
jw2013 <zhew@live.unc.edu>
2014-03-01 20:55:02 -0500

    still a piece of shit

--- api/proxy.php
+++ api/proxy.php
@@ -1,4 +1,15 @@
 <?php
+ini_set('display_errors', 'On');
+
+// INITIALIZE IT
+// phpQuery::newDocumentHTML($markup);
+// phpQuery::newDocumentXML();
+// phpQuery::newDocumentFileXHTML('test.html');
+// phpQuery::newDocumentFilePHP('test.php');
+// phpQuery::newDocument('test.xml', 'application/rss+xml');
+// this one defaults to text/html in utf8
+
+
 function get_data($url) {
 	$ch = curl_init();
 	$timeout = 5;
@@ -10,11 +21,35 @@ function get_data($url) {
 	return $data;
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['url'])) {
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['url']) && isset($_GET['filepath'])) {
 
 $url = $_GET['url'];
+$filepath = $_GET['filepath'];
 
 $returned_content = get_data($url);
+// echo $doc['a']->attr('src', "999");
+// $doc['a']->attr('href', "qwert88888qwert");
+
+  $dom = new DomDocument();
+  $dom->loadHTML($returned_content);
+  $A = array();
+  $refs = $dom->getElementsByTagName('script');
+  foreach ($refs as $book) {
+  	array_push($A, $book->getAttribute('src'));
+  }
+
+   $refs = $dom->getElementsByTagName('link');
+  foreach ($refs as $book) {
+  	array_push($A, $book->getAttribute('href'));
+  }
+
+  $relative_address = '../shared/' . $filepath;
+  if (!is_dir($relative_address))
+  {
+    mkdir($relative_address, 0755, true);
+  }
+  
 
 // $regex = '/(script|link)*(href|src)[^ ]*/';
 
@@ -27,12 +62,12 @@ $returned_content = get_data($url);
 //    print('<br>');
 // }
 
-$A = array("/cs.unc.edu/cms/unctheme/static/xdvprint.css", '/cs.unc.edu/cms/portal_css/Oasis%20Onetheme/people-cachekey4934.css');
 
 for($i = 0; $i < count($A); $i++)
 { 
      $data = get_data($A[$i]);
-     $dir_name = '.';
+     
+     $dir_name = $relative_address;
      $dirs = explode('/', $A[$i]);
      for($j = 0; $j < count($dirs) - 1; $j++){
        if($dirs[$j] != ""){
@@ -44,6 +79,8 @@ for($i = 0; $i < count($A); $i++)
        }
      }
      $filename = $dir_name . '/' . $dirs[count($dirs) - 1];
+     print($filename);
+     print('<br>');
      $fp = fopen($filename, 'w');
 	 fwrite($fp, $data);
 	 fclose($fp);
--- index.php
+++ index.php
@@ -1,6 +1,75 @@
+<?php if(isset($_GET['file']) == false){
+    $rand = substr(base64_encode(sha1(time() . rand())), 0,8);
+    header("Location: index.php?file=" . $rand);
+}?>
 <!DOCTYPE html>
-<head></head>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <title> Shared Proxy </title>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.1/jquery.min.js" type="text/javascript"></script>
+
+
+</head>
+
 <body>
-<iframe src="http://www.w3schools.com"></iframe>
+
+<h1>Shared Proxy</h1>
+
+<div>
+    <input type="text" id="urlpath" name="urlpath" size="100">
+    <button id="Go"> Go </button>
+</div>
+    
+
+<div id="sharedBoard" width="1024" height="768">
+</div>
+
+<script type="text/javascript">
+
+$(document).ready(function(){
+
+    $("#Go").on("click", function(){
+        updateURL($("#urlpath").val());
+    });
+
+});
+
+
+var updateURL = function (URLconnector){
+    var params = window.location.search.substr(1).split('=');
+    var filepath = "";
+    for(var i = 0; i < params.length; i++){
+        if(params[i] === "file") {
+            filepath = params[i + 1];
+            break;
+        }
+    }
+
+    $.ajax('api/proxy.php', {
+        type: 'GET',
+        dataType : 'html',
+        data : {
+          url : URLconnector,
+          filepath : filepath
+        },
+        success: function (data, textStatus, jqXHR){
+            $('#sharedBoard').html(data);
+        },
+        error: function (jqXHR, textStatus, errorThown){
+            alert('Load failed');
+        },
+        cache:false
+    });
+};
+</script>
+
+
 </body>
 </html>
+
+
+<html>
+<body>
